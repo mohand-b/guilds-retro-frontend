10bb829ca29b2a43c54a7fa397de9860079a727c
mohand-b <mohandbdj@gmail.com>
2024-08-30 17:42:26 +0200

    refactor(routing): update user profile route

--- src/app/routes/profile/profile.routes.ts
+++ src/app/routes/profile/profile.routes.ts
@@ -5,8 +5,4 @@ export const ProfileRoutes: Route[] = [
     path: '',
     loadComponent: () => import('./containers/profile/profile.component').then(c => c.ProfileComponent)
   },
-  {
-    path: ':username',
-    loadComponent: () => import('./containers/profile/profile.component').then(c => c.ProfileComponent)
-  }
 ]
--- src/app/routes/registry/registry.routes.ts
+++ src/app/routes/registry/registry.routes.ts
@@ -5,4 +5,8 @@ export const RegistryRoutes: Route[] = [
     path: '',
     loadComponent: () => import('./containers/users-registry/users-registry.component').then(c => c.UsersRegistryComponent),
   },
+  {
+    path: 'user/:username',
+    loadComponent: () => import('../profile/containers/profile/profile.component').then(c => c.ProfileComponent),
+  }
 ];
