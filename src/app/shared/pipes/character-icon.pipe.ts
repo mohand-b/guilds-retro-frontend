fe727af30f5c70333b2c2ceb8c328d3d3756f895
mohand-b <mohandbdj@gmail.com>
2024-09-24 22:50:36 +0200

    feat(pipes): add new icons and create characterUnisexIcon and characterColor pipes

--- src/app/shared/pipes/character-icon.pipe.ts
+++ src/app/shared/pipes/character-icon.pipe.ts
@@ -1,5 +1,5 @@
 import {Pipe, PipeTransform} from '@angular/core';
-import {UserDto} from "../../routes/profile/state/users/user.model";
+import {CharacterClassEnum, UserDto} from "../../routes/profile/state/users/user.model";
 
 @Pipe({
   name: 'characterIcon',
@@ -16,6 +16,65 @@ export class CharacterIconPipe implements PipeTransform {
     const gender = user.gender.toLowerCase();
 
     return `assets/characters/${characterClass}-${gender}-icon.svg`;
+  }
+}
+
+@Pipe({
+  name: 'characterUnisexIcon',
+  standalone: true
+})
+export class CharacterUnisexIconPipe implements PipeTransform {
+
+  transform(user: UserDto): string {
+    if (!user || !user.characterClass) {
+      return '';
+    }
+
+    const characterClass = user.characterClass.toLowerCase
+
+    return `assets/characters/${characterClass}_icon.svg`;
+  }
+}
+
+@Pipe({
+  name: 'characterColor',
+  standalone: true
+})
+export class CharacterColorPipe implements PipeTransform {
+
+  transform(user: UserDto): string {
+    if (!user || !user.characterClass) {
+      return '';
+    }
+
+    switch (user.characterClass) {
+      case CharacterClassEnum.CRA:
+        return '#D91616';
+      case CharacterClassEnum.ECAFLIP:
+        return '#F8F2AB';
+      case CharacterClassEnum.ENIRIPSA:
+        return '#EB5B6F';
+      case CharacterClassEnum.ENUTROF:
+        return '#EBCA00';
+      case CharacterClassEnum.FECA:
+        return '#a9946a';
+      case CharacterClassEnum.IOP:
+        return '#ED8502';
+      case CharacterClassEnum.OSAMODAS:
+        return '#1DA9E2';
+      case CharacterClassEnum.PANDAWA:
+        return '#4C4F42';
+      case CharacterClassEnum.SACRIEUR:
+        return '#C3D214';
+      case CharacterClassEnum.SADIDA:
+        return '#3BAA35';
+      case CharacterClassEnum.SRAM:
+        return '#3B4193';
+      case CharacterClassEnum.XELOR:
+        return '#026491';
+      default:
+        return '#BBBBBB';
+    }
   }
 }
 
