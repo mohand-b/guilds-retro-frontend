bfcaf4ee73fa5c21cdf32cbc1cb46942a0fa4b2f
mohand-b <mohandbdj@gmail.com>
2024-10-15 11:46:39 +0200

    refactor(ui): migrate registry container to PrimeNG

--- src/app/routes/registry/containers/registry/registry.component.ts
+++ src/app/routes/registry/containers/registry/registry.component.ts
@@ -1,6 +1,7 @@
 import {Component, inject} from '@angular/core';
 import {Router, RouterLink, RouterOutlet} from "@angular/router";
-import {MatButtonModule} from "@angular/material/button";
+import {SelectButtonModule} from "primeng/selectbutton";
+import {FormControl, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-registry',
@@ -8,17 +9,25 @@ import {MatButtonModule} from "@angular/material/button";
   imports: [
     RouterOutlet,
     RouterLink,
-    MatButtonModule
+    SelectButtonModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './registry.component.html',
   styleUrl: './registry.component.scss'
 })
 export class RegistryComponent {
 
+  navOptions = [
+    {label: 'Utilisateurs', route: '/registry/users', icon: 'pi pi-user'},
+    {label: 'Guildes', route: '/registry/guilds', icon: 'pi pi-shield'},
+  ];
+
   private router = inject(Router);
 
-  isActive(route: string): boolean {
-    return this.router.url.includes(route);
+  navigationControl = new FormControl(this.router.url);
+
+  onChange(route: any) {
+    this.router.navigate([route]);
   }
 
 }
