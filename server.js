1c704cc8e1ff4e349138742e14bc87e4f7f7ac4f
mohand-b <mohandbdj@gmail.com>
2024-11-06 11:10:03 +0100

    deploy application on Heroku

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist/guilds-retro-frontend')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/guilds-retro-frontend/index.html'));
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
