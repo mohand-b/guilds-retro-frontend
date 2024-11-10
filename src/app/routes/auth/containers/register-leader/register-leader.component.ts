061bd22d5ac8dad3ed03871e112077339f79ca09
mohand-b <mohandbdj@gmail.com>
2024-11-10 15:57:08 +0100

    feat(register-leader): redesign register leader

--- src/app/routes/auth/containers/register-leader/register-leader.component.ts
+++ src/app/routes/auth/containers/register-leader/register-leader.component.ts
@@ -1,21 +1,44 @@
 import {Component, inject, OnInit} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
-import {GuildSelectedCardComponent} from "../../components/guild-selected-card/guild-selected-card.component";
-import {CommonModule, Location} from "@angular/common";
-import {AuthFacade} from "../../auth.facade";
-import {GuildFacade} from "../../../guild/guild.facade";
-import {toFormData} from "../../../../shared/extensions/object.extension";
-import {Router} from "@angular/router";
-import {distinctUntilChanged, finalize} from "rxjs";
+import {CommonModule, Location} from '@angular/common';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators
+} from '@angular/forms';
+import {finalize} from 'rxjs/operators';
+import {Router} from '@angular/router';
+import {StepperModule} from "primeng/stepper";
 import {AlertComponent} from "../../../../shared/components/alert/alert.component";
-import {CharacterClassEnum, GenderEnum} from "../../../profile/state/users/user.model";
-import {InputTextModule} from "primeng/inputtext";
 import {ButtonModule} from "primeng/button";
+import {PasswordModule} from "primeng/password";
 import {SliderModule} from "primeng/slider";
 import {DropdownModule} from "primeng/dropdown";
-import {PasswordModule} from "primeng/password";
-import {InputIconModule} from "primeng/inputicon";
+import {AuthFacade} from "../../auth.facade";
+import {GuildFacade} from "../../../guild/guild.facade";
+import {CharacterClassEnum, GenderEnum, UserDto} from "../../../profile/state/users/user.model";
+import {toFormData} from "../../../../shared/extensions/object.extension";
+import {InputTextModule} from "primeng/inputtext";
 import {IconFieldModule} from "primeng/iconfield";
+import {InputIconModule} from "primeng/inputicon";
+import {CharacterIconPipe} from "../../../../shared/pipes/character-icon.pipe";
+import {CheckboxModule} from "primeng/checkbox";
+import {GenericModalService} from "../../../../shared/services/generic-modal.service";
+import {CguContentComponent} from "../../../terms/components/cgu-content/cgu-content.component";
+
+function bothCheckboxesChecked(): ValidatorFn {
+  return (form: AbstractControl): ValidationErrors | null => {
+    const acceptCgu = form.get('acceptCgu')?.value;
+    const confirmInfoAccuracy = form.get('confirmInfoAccuracy')?.value;
+
+    return acceptCgu && confirmInfoAccuracy ? null : {bothRequired: true};
+  };
+}
 
 @Component({
   selector: 'app-register-leader',
@@ -23,7 +46,6 @@ import {IconFieldModule} from "primeng/iconfield";
   imports: [
     CommonModule,
     FormsModule,
-    GuildSelectedCardComponent,
     ReactiveFormsModule,
     AlertComponent,
     InputTextModule,
@@ -32,40 +54,56 @@ import {IconFieldModule} from "primeng/iconfield";
     DropdownModule,
     PasswordModule,
     InputIconModule,
-    IconFieldModule
+    IconFieldModule,
+    StepperModule,
+    CharacterIconPipe,
+    CheckboxModule
   ],
   templateUrl: './register-leader.component.html',
-  styleUrl: './register-leader.component.scss'
+  styleUrls: ['./register-leader.component.scss']
 })
 export class RegisterLeaderComponent implements OnInit {
 
-  public registerAsLeaderForm: FormGroup;
+  public characterInfoForm: FormGroup;
+  public guildInfoForm: FormGroup;
+  public confirmationForm: FormGroup;
   public validateGuildCodeControl: FormControl;
   public logoPreview: string | ArrayBuffer | null = null;
   isLoading = false;
+
   protected readonly characterClasses: CharacterClassEnum[] = Object.values(CharacterClassEnum);
   protected readonly GenderEnum = GenderEnum;
+
   private fb: FormBuilder = inject(FormBuilder);
   private authFacade = inject(AuthFacade);
   private guildFacade = inject(GuildFacade);
+  private genericModalService = inject(GenericModalService);
   private location: Location = inject(Location);
   private router = inject(Router);
 
   constructor() {
-    this.registerAsLeaderForm = this.fb.group({
-      username: this.fb.control<string>('', [
-        Validators.required,
-        Validators.minLength(4),
-      ]),
-      password: this.fb.control<string>('', [Validators.required, Validators.minLength(4)]),
-      characterClass: this.fb.control<CharacterClassEnum | null>(null, [Validators.required]),
-      characterLevel: this.fb.control<number>(1, [Validators.required]),
-      gender: this.fb.control<GenderEnum>(GenderEnum.MALE, [Validators.required]),
-      guildName: this.fb.control<string>('', [Validators.required]),
-      level: this.fb.control<number>(1, [Validators.required]),
-      description: this.fb.control<string>(''),
-      logo: this.fb.control<File | null>(null),
+    this.characterInfoForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(4)]],
+      password: ['', [Validators.required, Validators.minLength(4)]],
+      characterClass: [null, [Validators.required]],
+      characterLevel: [1, [Validators.required]],
+      gender: [GenderEnum.MALE, [Validators.required]]
     });
+
+    this.guildInfoForm = this.fb.group({
+      guildName: ['', [Validators.required]],
+      level: [1, [Validators.required]],
+      description: [''],
+      logo: [null]
+    });
+
+    this.confirmationForm = this.fb.group(
+      {
+        acceptCgu: [{value: false, disabled: true}, Validators.requiredTrue],
+        confirmInfoAccuracy: [false, Validators.requiredTrue]
+      },
+      {validators: bothCheckboxesChecked()}
+    );
 
     this.validateGuildCodeControl = this.fb.control<string>('', [
       Validators.required,
@@ -74,52 +112,64 @@ export class RegisterLeaderComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.validateGuildCodeControl.valueChanges.pipe(
-      distinctUntilChanged()
-    ).subscribe(() => {
-      this.validateGuildCodeControl.patchValue(this.validateGuildCodeControl.value.toUpperCase());
-    });
+    this.validateGuildCodeControl.valueChanges
+      .pipe(finalize(() => this.isLoading = false))
+      .subscribe(() => {
+        this.validateGuildCodeControl.patchValue(this.validateGuildCodeControl.value.toUpperCase(), {emitEvent: false});
+      });
   }
 
   onSubmit(): void {
-    if (this.registerAsLeaderForm.invalid) return;
+    if (this.characterInfoForm.invalid || this.guildInfoForm.invalid) return;
     this.isLoading = true;
-    this.authFacade.registerAsLeader(toFormData(this.registerAsLeaderForm.value))
-      .pipe(
-        finalize(() => this.isLoading = false)
-      )
+
+    const formData = {
+      ...this.characterInfoForm.value,
+      ...this.guildInfoForm.value
+    };
+
+    this.authFacade.registerAsLeader(toFormData(formData))
+      .pipe(finalize(() => this.isLoading = false))
       .subscribe({
         next: () => this.router.navigate(['/']),
         error: (error) => {
           if (error.status === 409) {
-            this.registerAsLeaderForm.get('username')?.setErrors({usernameAlreadyTaken: true});
+            this.characterInfoForm.get('username')?.setErrors({usernameAlreadyTaken: true});
           }
         }
       });
   }
 
   public selectGender(gender: GenderEnum): void {
-    this.registerAsLeaderForm.patchValue({gender});
+    this.characterInfoForm.patchValue({gender});
   }
 
+  get previewCharacterClass(): Pick<UserDto, 'characterClass' | 'gender'> {
+    return {
+      characterClass: this.characterInfoForm.value.characterClass,
+      gender: this.characterInfoForm.value.gender
+    }
+  }
 
   validateGuildCode() {
     this.guildFacade.validateGuildCode(this.validateGuildCodeControl.value).subscribe({
-      next: (res) => {
-        this.registerAsLeaderForm.patchValue({guildName: res.guildName});
-      },
+      next: (res) => this.guildInfoForm.patchValue({guildName: res.guildName}),
       error: (error) => {
         if (error.status === 404) {
           this.validateGuildCodeControl.setErrors({invalidGuildCode: true});
-          return;
         }
       }
     });
   }
 
+  validateAndPreventSubmit(event: any) {
+    event.preventDefault();
+    this.validateGuildCode();
+  }
+
   onImportLogo(event: Event): void {
     const file = (event.target as HTMLInputElement).files?.[0] || null;
-    this.registerAsLeaderForm.patchValue({logo: file});
+    this.guildInfoForm.patchValue({logo: file});
     if (file) {
       const reader = new FileReader();
       reader.onload = () => {
@@ -129,7 +179,28 @@ export class RegisterLeaderComponent implements OnInit {
     }
   }
 
+  onOpenCgu() {
+    const dialogRef = this.genericModalService.open(
+      "Conditions générales d'utilisation",
+      {'primary': "J'ai lu et j'accepte"},
+      'md',
+      null,
+      CguContentComponent,
+      undefined,
+      false,
+      false
+    )
+
+    dialogRef.onClose.subscribe((read) => {
+      if (read) {
+        this.confirmationForm.get('acceptCgu')?.setValue(true);
+        this.confirmationForm.get('acceptCgu')?.enable();
+      }
+    })
+  }
+
   goBack() {
     this.location.back();
   }
+
 }
